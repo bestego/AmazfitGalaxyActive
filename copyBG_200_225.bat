dcb56f2e94693dddd92bca193317a84d8809c98b
Erik van Gompel <erik@van-gompel.nl>
2019-09-20 17:49:26 +0200

    Initial version. Todo:optimize image usage, connection, dnd

---
+++ copyBG_200_225.bat
@@ -0,0 +1,20 @@
+copy /y 0000.png 0200.png
+copy /y 0000.png 0201.png
+copy /y 0000.png 0202.png
+copy /y 0000.png 0203.png
+copy /y 0000.png 0204.png
+copy /y 0000.png 0205.png
+copy /y 0000.png 0206.png
+copy /y 0000.png 0207.png
+copy /y 0000.png 0208.png
+copy /y 0000.png 0209.png
+copy /y 0000.png 0210.png
+
+copy /y 0000.png 0220.png
+copy /y 0000.png 0221.png
+copy /y 0000.png 0222.png
+copy /y 0000.png 0223.png
+copy /y 0000.png 0224.png
+copy /y 0000.png 0225.png
+
+pause
---
+++ copy_0090.bat
@@ -0,0 +1,11 @@
+REM copy index 0090.png to 0091.png .. 0099.png
+copy /b /y 0090.png 0091.png
+copy /b /y 0090.png 0092.png
+copy /b /y 0090.png 0093.png
+copy /b /y 0090.png 0094.png
+copy /b /y 0090.png 0095.png
+copy /b /y 0090.png 0096.png
+copy /b /y 0090.png 0097.png
+copy /b /y 0090.png 0098.png
+copy /b /y 0090.png 0099.png
+pause
